3e459a4d6f08f1534464d9fcced700b6184b45d3
sekodevbrebes <sekodevbrebes@gmail.com>
2025-12-07 15:56:26 +0700

    update file FilterButton

--- src/components/menu/FilterButton.tsx
+++ src/components/menu/FilterButton.tsx
@@ -1,15 +1,26 @@
 "use client";
 
-export default function FilterButton({ label, active, value, onClick }) {
-  
+// ✅ Tipe props
+type FilterButtonProps = {
+  label: string;
+  value: string;
+  active: boolean;
+  onClick: (value: string) => void;
+};
+
+export default function FilterButton({
+  label,
+  active,
+  value,
+  onClick,
+}: FilterButtonProps) {
   return (
     <button
-      className={`px-5 py-2 font-medium rounded-2xl cursor-pointer ${
+      className={`px-5 py-2 font-medium rounded-2xl cursor-pointer transition-all ${
         active
           ? "bg-amber-700 text-white"
           : "bg-amber-100 text-amber-700 hover:bg-amber-200"
       }`}
-     
       onClick={() => onClick(value)}
     >
       {label}
